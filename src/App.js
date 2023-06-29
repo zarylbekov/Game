a29715db73153cb836b37fca27122326c2886a83
Sora <nurbekzarylbekov52@gmail.com>
2023-06-29 14:54:00 +0600

    Game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
+function Square(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
-export default App;
+class Board extends React.Component {
+ constructor(props) {
+  super(props)
+  this.state = {
+    squares: Array(9). fill(null),
+    xIsNext: true
+  }
+ }
+
+handleClick(i) {
+  const squares = this.state.squares.slice()
+  if(calculateWinner(squares) || squares[i]) {
+    return
+  }
+  squares[i] = this.state.xIsNext ? "X" : "O";
+  this.setState({
+    squares:squares,
+    xIsNext: !this.state.xIsNext,
+  })
+}
+
+  renderSquare(i){
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
+  }
+
+  render(){
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = "winner:" + winner
+    }else {
+      status = "Next player" + (this.state.xIsNext ? "X" : "O")
+    }
+
+    return(
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render(){
+    return(
+      <div className="game">
+        <div className="gamme-board">
+        <Board/>
+        </div>
+        <div className="game-info">
+          <div></div>
+          <ol></ol>
+        </div>
+      </div>
+    );
+  }
+}  
+
+export default Game;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i=i+1) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+    return squares[a]
+    }
+  }
+  return null;
+}
